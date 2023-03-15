75df5e7e84599f75dc97aa2346e6f5245b5df95c
pateldd1 <devanshpatel22@gmail.com>
2023-03-15 18:05:32 -0400

    some cleanup

--- src/components/Bookmarks.jsx
+++ src/components/Bookmarks.jsx
@@ -5,14 +5,14 @@ import Grid2 from "@mui/material/Unstable_Grid2";
 import RecipeCard from "./RecipeCard";
 
 export default function Bookmarks() {
-  const value = useSelector(selectBookmarks);
+  const bookMarkedMeals = useSelector(selectBookmarks);
 
   return (
     <div>
       <Grid2 container spacing={3}>
         <Grid2 xs={12}>
           <div style={{ display: "flex" }}>
-            {value.map((meal) => (
+            {bookMarkedMeals.map((meal) => (
               <div key={meal.idMeal} style={{ margin: "10px" }}>
                 <RecipeCard details={meal} isBookmark={false} />
               </div>
